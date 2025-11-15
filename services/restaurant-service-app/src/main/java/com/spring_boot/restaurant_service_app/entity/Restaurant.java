55a6a47020c8b55896576f318d5c5e3e00135154
dheeraj <pilladheeraj@gmail.com>
2025-11-15 12:54:42 +0530

    Remove unused java.awt import from Restaurant.java
    Eliminated an unnecessary import of java.awt to clean up the Restaurant entity class.

--- services/restaurant-service-app/src/main/java/com/spring_boot/restaurant_service_app/entity/Restaurant.java
+++ services/restaurant-service-app/src/main/java/com/spring_boot/restaurant_service_app/entity/Restaurant.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.awt.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
